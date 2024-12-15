0d34283594a018264fffeff74f0dbc84fbe1efa6
janipang <pang.janista@gmail.com>
2024-12-15 15:04:11 +0700

    feat(api): get-user

--- app/app.py
+++ app/app.py
@@ -1,11 +1,21 @@
 
-from flask import Flask, jsonify
+from flask import Flask, json, jsonify
 
 app = Flask(__name__)
 
+def getUsersFromDB():
+  with open("./users.json", encoding="utf-8") as json_file:
+      parsed_json = json.load(json_file)
+      return parsed_json
+    
 @app.route('/')
 def index():
     return "Hello, Welcome to our simple flask api"
+
+@app.route('/user', methods=['GET'])
+def getUser():
+  user = getUsersFromDB()
+  return jsonify(user)
 
 # your code here
 # ...
